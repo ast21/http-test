29873cac2c6f1a10b54cedba5c41e27bbc8bcbf5
Anastas Mironov <ast@l80.ru>
2024-09-09 01:35:43 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	var err error
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	data := make(map[string]string)
+	data["hostname"], err = os.Hostname()
+	if err != nil {
+		data["hostname"] = "unknown"
+	}
+
+	jsonData, _ := json.Marshal(data)
+
+	w.Write(jsonData)
+}
+
+func main() {
+	http.HandleFunc("/", mainHandler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
